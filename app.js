fcacca6901e799281bef5c38e109433ec7ac78b6
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-02-11 09:48:44 +0430

    init

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ const video = document.querySelector('.video-container')
 btn.addEventListener('click', function() {
     if(!btn.classList.contains('slide')) {
         btn.classList.add('slide')
+        video.pause()
     }
     else {
         btn.classList.remove('slide')
+        video.play()
     }
 })
